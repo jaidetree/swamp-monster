0f2c71144a086063a4bbc241042bebde1d994dc7
jaide <jayzawrotny@gmail.com>
2026-02-01 21:33:11 -0500

    Progress on site layout and home page

--- lib/swamp_web/components/layouts.ex
+++ lib/swamp_web/components/layouts.ex
@@ -35,38 +35,41 @@ defmodule SwampWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
-        </a>
-      </div>
-      <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-4 items-center">
-          <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
-            <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
-          </li>
-        </ul>
+    <header class="px-4 sm:px-6 lg:px-8 absolute w-full">
+      <div class="container navbar">
+        <div class="flex-1">
+          <a href="/" class="flex-1 flex w-fit items-center gap-2 text-2xl text-white/75 hover:text-white transition-color duration-300">
+            <span class="font-logo">Swamp Monster Leather</span>
+          </a>
+        </div>
+        <div class="flex-none">
+          <ul class="flex flex-column px-1 space-x-8 items-center uppercase text-base text-white/75">
+            <li>
+              <a href="https://phoenixframework.org/" class="hover:text-white transition-color duration-300">Work</a>
+            </li>
+            <li>
+              <a href="https://phoenixframework.org/" class="hover:text-white transition-color duration-300">Training</a>
+            </li>
+            <li>
+              <a href="https://phoenixframework.org/" class="hover:text-white transition-color duration-300">Contact</a>
+            </li>
+            <li>
+              <a href="https://phoenixframework.org/" class="hover:text-white transition-color duration-300">Resources</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
 
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl space-y-4">
-        {render_slot(@inner_block)}
-      </div>
+    <main class="flex-grow">
+      {render_slot(@inner_block)}
     </main>
+
+    <footer class="bg-black py-4 text-xs">
+      <div class="container">
+        &copy; Copyright 2026 by Swamp Monster Leather
+      </div>
+    </footer>
 
     <.flash_group flash={@flash} />
     """
--- todos.md
+++ todos.md
@@ -1,6 +1,6 @@
 # To Do Items
 
-## Phase 1: Initial Setup
+## Phase 1: Static Setup
 
 ### Start Project
 
@@ -10,6 +10,36 @@
 - [x] Install Elixir, Postgres, and Node
 - [x] Create Phoenix app
 - [x] Create swamp_dev database
+
+### Base layout
+
+- [x] Remove light mode toggle and default to dark mode
+- [ ] Implement layout elements
+- [ ] Implement site footer
+- [ ] Implement navbar
+
+### Home page
+
+- [ ] Implement hero image
+- [ ] Implement works section
+- [ ] Implement home footer section
+- [ ] Implement training section
+- [ ] Implement resources section
+- [ ] Implement about section
+
+### Implement contact page
+
+- [ ] Define PageController
+- [ ] Define route to PageController.contact
+- [ ] Create HTML form & styles
+- [ ] Send email to swamp monster email
+
+### Implement works page
+
+- [ ] Define route to PageController.works
+- [ ] Define HTML & styles
+
+## Phase 2: Setup CMS
 
 ### Create Contexts
 
